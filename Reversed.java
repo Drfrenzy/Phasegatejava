83aa45d3cb43c3b48121c0d56e796dbe1d3dfe7b
Drfrenzy <owolarafeibrahim@gmail.com>
2024-10-03 12:27:04 +0100

    Reversed.java

---
+++ Reversed.java
@@ -0,0 +1,13 @@
+       public class Reversed{
+	public static void main(String...args){
+       
+
+        String [] words = {"There", "is", "a", "tide", "in", "the", "affairs", "of", "men"};
+          for (int redo = words.length - 1; redo >= 0; redo--) {
+          
+          System.out.print(words[redo] + " ");
+
+      }
+}
+
+   }
